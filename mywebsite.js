12f57018d5cf8c6b4848df43aeeba2da18d9a963
EmuraRyota <emuryo.150327@ezweb.ne.jp>
2023-11-14 23:06:46 +0900

    confirmボタン追加

--- mywebsite.js
+++ mywebsite.js
@@ -1,3 +1,11 @@
 const func1 = () => {
     alert('こんにちは！！')
 }
+
+const func2 = () => {
+    if (confirm("OK？")){
+        window.location.href = "myWebsite2.html"
+    }else{
+        alert('OKを押してくれないのか...')
+    }
+}
